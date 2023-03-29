1c69be112e578c604b7ae6cb35e5aa2cf788559d
AnnisaFitry <2041720123@student.polinema.ac.id>
2023-03-29 07:48:08 +0700

    foldering

--- Accumulator/Accumulator.py
+++ Accumulator/Accumulator.py
@@ -1,4 +1,5 @@
+from pyspark import SparkContext as sc
 myaccum = sc.accumulator(0)
 myrdd = sc.parallelize(range(1,100))
 myrdd.foreach(lambda value: myaccum.add(value))
-print myaccum.value
+print (myaccum.value)
